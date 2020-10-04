3d0553b3678757d39fa8a7c089ccb17c6574bb8a
Joe Fowler <scrumplesplunge@gmail.com>
2020-10-04 14:46:21 +0100

    Upgrade to verbose debug symbols.

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 CXXFLAGS += -std=c++17 -Wall -Wextra -pedantic
 opt: CXXFLAGS += -ffunction-sections -fdata-sections -flto -O3 -march=native
-debug: CXXFLAGS += -O0 -g
+debug: CXXFLAGS += -O0 -g3
 
 LDFLAGS += -fuse-ld=gold
 opt: LDFLAGS += -s -Wl,--gc-sections -flto -Ofast
